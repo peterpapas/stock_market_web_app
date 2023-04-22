cbb2e9c46a6eb2c93897d189b51c26835816a9dc
peterpapas <30211228+peterpapas@users.noreply.github.com>
2023-04-22 14:11:49 +1000

    updated markdown

--- stock_app.py
+++ stock_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from PIL import Image
 import yfinance as yf
 from datetime import datetime
+import base64
 
 
 # Set Streamlit page configuration
@@ -13,32 +14,60 @@ st.set_page_config(
     page_icon="./image.jpg"
 )
 
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    return f"data:image/png;base64,{encoded_string}"
+
+
 # Custom CSS
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 # Title & Image
 
-st.markdown("""
+encoded_image = image_to_base64("./image.jpg")
+
+st.markdown(f"""
     <style>
-        .title {
+        .container {{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            max-width: 1200px;
+            margin-left: auto;
+            margin-right: auto;
+            padding-left: 1rem;
+            padding-right: 1rem;
+        }}
+        .title {{
             font-weight: bold;
             font-size: 30px;
             margin-bottom: 10px;
-        }
-        .subtitle {
+            text-align: center;
+        }}
+        .subtitle {{
             font-size: 16px;
             margin-bottom: 20px;
-        }
+            text-align: center;
+        }}
+        img {{
+            max-width: 100%;
+            height: auto;
+        }}
     </style>
-    <div class="title">Stock Market Web Application in Python</div>
-    <div class="subtitle">
-        This web application provides real-time stock data visualization using Yahoo Finance. Users can enter a stock symbol, along with custom start and end dates, to view historical stock data, including the stock's closing price and trading volume. The application also displays key statistics on the selected stock data, providing insights into the stock's performance over the specified time period.
+    <div class="container">
+        <div class="title">Stock Market Web Application in Python</div>
+        <div class="subtitle">
+            This web application provides real-time stock data visualization using Yahoo Finance. Users can enter a stock symbol, along with custom start and end dates, to view historical stock data, including the stock's closing price and trading volume. The application also displays key statistics on the selected stock data, providing insights into the stock's performance over the specified time period.
+        </div>
+        <img src="{encoded_image}" alt="Stock Market">
     </div>
 """, unsafe_allow_html=True)
 
-image = Image.open("./image.jpg")
-st.image(image, use_column_width=True)
 
 # side bar header
 st.sidebar.header('User Input')
